de59dc313a0d5ef326e2417b5732a299f8cd231b
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-30 19:10:20 -0700

    notes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const { type } = require("os");
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -12,3 +13,36 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+//!instructor questions
+
+// Are we generating a template or a full README
+    //Thinking of building the blocks of information for user to fill in later
+    //vs
+    //Building the README creator where the user only interacts with the command line
+
+    My thought is to create the a 2 part questionaire. 
+    Part 1
+        Author's name, 
+        GitHub user ID
+    Part 2
+    1. What type of content is this?
+        Headers
+        Emphasis
+        List - This might be dificult. Would have to know how to separate the list items 
+        Assuming list is an option - List by type.
+        Links
+        Table would be cool but difficult.
+
+    2. Content - this question would have to be adaptable depending on the type of content.
+    3. Would you like to add more content (repeat loop)
+        
+//!Options
+//Headers
+//Emphasis
+//Lists - ordered/unordered
+//Images
+//Links
+//Tables
+

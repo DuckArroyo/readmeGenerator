4775ad7504a0e74b067247dd429e73bb0c3d2919
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-09-30 19:11:01 -0700

    push notes

--- index.js
+++ index.js
@@ -14,30 +14,29 @@ function init() {}
 // Function call to initialize app
 init();
 
-
 //!instructor questions
 
 // Are we generating a template or a full README
-    //Thinking of building the blocks of information for user to fill in later
-    //vs
-    //Building the README creator where the user only interacts with the command line
+//Thinking of building the blocks of information for user to fill in later
+//vs
+//Building the README creator where the user only interacts with the command line
 
-    My thought is to create the a 2 part questionaire. 
-    Part 1
-        Author's name, 
-        GitHub user ID
-    Part 2
-    1. What type of content is this?
-        Headers
-        Emphasis
-        List - This might be dificult. Would have to know how to separate the list items 
-        Assuming list is an option - List by type.
-        Links
-        Table would be cool but difficult.
+// My thought is to create the a 2 part questionaire.
+// Part 1
+//     Author's name,
+//     GitHub user ID
+// Part 2
+// 1. What type of content is this?
+//     Headers
+//     Emphasis
+//     List - This might be dificult. Would have to know how to separate the list items
+//     Assuming list is an option - List by type.
+//     Links
+//     Table would be cool but difficult.
 
-    2. Content - this question would have to be adaptable depending on the type of content.
-    3. Would you like to add more content (repeat loop)
-        
+// 2. Content - this question would have to be adaptable depending on the type of content.
+// 3. Would you like to add more content (repeat loop)
+
 //!Options
 //Headers
 //Emphasis
@@ -45,4 +44,3 @@ init();
 //Images
 //Links
 //Tables
-
